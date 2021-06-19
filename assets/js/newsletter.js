170c8a9d527e729138ba8d6bf72bbd0fe30315e8
serverman1997 <86143260+serverman1997@users.noreply.github.com>
2021-06-19 15:32:41 +0530

    Add files via upload

---
+++ assets/js/newsletter.js
@@ -0,0 +1,63 @@
+function subscribe() {
+
+    var newsletteremail = document.getElementById('newsletteremail').value;
+    var endpointurl = "https://script.google.com/macros/s/XeG2_8bVGs_cA1Ir0P8h7CQS-qlSG/exec?func=addData&email=" + newsletteremail;
+  
+    var emailpatt = new RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/);
+    if (navigator.onLine) {
+  
+  
+      if (newsletteremail == "" || emailpatt.test(newsletteremail) == false) {
+        Swal.fire({
+          type: 'error',
+          title: 'Oops...',
+          text: 'Email is not valid, Please provide the valid email',
+          confirmButtonColor: '#ff5821'
+        })
+  
+      }
+      else {
+        $('#cover-spin').show(30);
+        $.ajax({
+          type: 'GET',
+          url: endpointurl,
+          success: function (data) {
+  
+            if (data.status == true) {
+              Swal.fire({
+                type: 'success',
+                title: 'Newsletter Subscribed successfully!!! for ' + data.email,
+                text: 'All the latest content from E-YARD will be dropped directly to your inbox.',
+                confirmButtonColor: '#ff5821'
+              })
+  
+              document.getElementById('newsletteremail').value = '';
+              $('#cover-spin').hide(30);
+            }
+            else {
+              Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong. Please try again later',
+                confirmButtonColor: '#ff5821'
+              })
+  
+            }
+          }
+  
+        });
+      }
+  
+    }
+    else { 
+      Swal.fire({
+        type: 'question',
+        title: 'The Internet?',
+        text: 'That thing is still around?',
+        confirmButtonColor: '#ff5821'
+      })
+      
+    }
+  
+  
+  }
